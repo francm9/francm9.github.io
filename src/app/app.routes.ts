f2210269972f377472dabd6aa0e8f932b992c7e3
Fco. Javier Cano Moreno <francano368@uma.es>
2025-11-29 23:52:11 +0100

    test

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { CalendarComponent } from './calendar/calendar.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-
-export const routes: Routes = [
-    { path: '', component: WelcomeComponent },
-    { path: 'calendar', component: CalendarComponent }
-];
